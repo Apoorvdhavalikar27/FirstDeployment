6e8caa0f35ca33a5c0d099f6256dd4dd4a38db2a
Apoorvdhavalikar27 <apoorvdhavalikar1@gmail.com>
2021-10-23 00:56:39 +0530

    first commit

--- myproject/settings.py
+++ myproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-8f5kl-74+wi(gh)y==(4fsb*ic31#1_85^jc39f(+usyc+i*2v
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["apoorvsapp.herokuapp.com", "localhost"]
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
